69aeaf45d81053acb53c4acf2e999a870ffd3e79
Caleb Hattingh <caleb.hattingh@gmail.com>
2019-12-21 20:53:18 +1000

    Added basic doc

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 
+/// Generates a hashmap of the dictionary definitions from a static
+/// resource compiled right into the executable.
 pub fn make_words() -> HashMap<String, String> {
-//    let v = include_str!("../data/dictionary.json");
     let v = include_str!("../dictomatic.json");
     serde_json::from_str(v).unwrap()
 }
