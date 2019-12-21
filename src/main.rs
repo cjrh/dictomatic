085582d357f252a9a1242d51338cd1146e376769
Caleb Hattingh <caleb.hattingh@gmail.com>
2019-12-22 01:38:49 +1000

    Improved arrangement, but we can still do better.

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,28 @@ use structopt::StructOpt;
 use std::io::{BufReader, BufRead};
 use fileinput::FileInput;
 use std::io;
+use crate::types::Definition;
+use serde_json;
 
 #[derive(StructOpt)]
 struct Cli {
     #[structopt(default_value = "")]
     word: String,
+}
+
+fn find_word(defns: &Vec<Definition>, word: String) -> String {
+    let r = match defns.binary_search_by_key(&word, |d| d.word.clone()) {
+        Ok(idx) => defns[idx].defns.clone(),
+        Err(_) => panic!("Not found")
+    };
+    r
+}
+
+fn print_defns(defns: &str) {
+    serde_json::from_str::<Vec<String>>(defns).unwrap()
+        .iter()
+        .filter(|&s| !s.trim().is_empty())
+        .for_each(|s| println!("{}", &s));
 }
 
 fn main() -> io::Result<()> {
@@ -23,10 +40,16 @@ fn main() -> io::Result<()> {
             let fileinput = FileInput::new(&filenames);
             let reader = BufReader::new(fileinput);
             for line in reader.lines() {
-                println!("{}", hm[&line.unwrap()]);
+//                println!("{}", line.unwrap());
+                let defns = find_word(&hm, line.unwrap().to_string());
+                print_defns(&defns);
+                println!("\n");
             }
-        },
-        _ => println!("{}", hm[&args.word]),
+        }
+        word => {
+            let defns = find_word(&hm, word.to_string());
+            print_defns(&defns);
+        }
     }
     Ok(())
 }
--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,21 @@
-use std::collections::HashMap;
+//use std::time::{SystemTime, UNIX_EPOCH};
+
+pub struct Definition {
+    pub word: String,
+    pub defns: String,
+}
 
 /// Generates a hashmap of the dictionary definitions from a static
 /// resource compiled right into the executable.
-pub fn make_words() -> HashMap<String, String> {
-    let v = include_str!("../dictomatic.json");
-    serde_json::from_str(v).unwrap()
+pub fn make_words() -> Vec<Definition> {
+//    let now = SystemTime::now();
+//    println!("{}", now.elapsed().unwrap().as_millis());
+    const V: &str = include_str!("../dictomatic.tsv");
+//    println!("{}", now.elapsed().unwrap().as_millis());
+    let out = V.lines().map(|line| {
+        let parts: Vec<&str> = line.splitn(2, "\t").collect();
+        Definition { word: parts[0].to_string(), defns: parts[1].to_string() }
+    }).collect();
+//    println!("{}", now.elapsed().unwrap().as_millis());
+    out
 }
