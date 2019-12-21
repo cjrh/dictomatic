7acfdc859003228636907072e57225ddd7da5b7d
Caleb Hattingh <caleb.hattingh@gmail.com>
2019-12-21 23:11:08 +1000

    Allow switching between args and reading from stdin

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,30 @@ mod types;
 use types::make_words;
 
 use structopt::StructOpt;
+use std::io::{BufReader, BufRead};
+use fileinput::FileInput;
+use std::io;
 
 #[derive(StructOpt)]
 struct Cli {
+    #[structopt(default_value = "")]
     word: String,
 }
 
-fn main() {
-    let args = Cli::from_args();
+fn main() -> io::Result<()> {
     let hm = make_words();
-    println!("{}", hm[&args.word]);
+    let args = Cli::from_args();
+    match &args.word as &str {
+        // If no word has been given as a parameter, read from stdin
+        "" => {
+            let filenames: Vec<&str> = vec![];
+            let fileinput = FileInput::new(&filenames);
+            let reader = BufReader::new(fileinput);
+            for line in reader.lines() {
+                println!("{}", hm[&line.unwrap()]);
+            }
+        },
+        _ => println!("{}", hm[&args.word]),
+    }
+    Ok(())
 }
