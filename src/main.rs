a08bfa26676286c057457ec8e78e3d506a130b7f
Caleb Hattingh <caleb.hattingh@gmail.com>
2026-06-02 15:44:25 +0200

    Update deps

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 mod types;
 
-use types::{make_words};
+use types::make_words;
 
-use structopt::StructOpt;
-use std::io::{BufReader, BufRead};
+use clap::Parser;
 use fileinput::FileInput;
 use std::io;
-use serde_json;
+use std::io::{BufRead, BufReader};
 
-#[derive(StructOpt)]
+#[derive(Parser)]
 struct Cli {
-    #[structopt(parse(from_str))]
     words: Vec<String>,
 }
 
-fn print_defns(defns: &Vec<&str>, idx: usize, word: &str) {
-    serde_json::from_str::<Vec<String>>(defns[idx]).unwrap()
+fn print_defns(defns: &[&str], idx: usize, word: &str) {
+    serde_json::from_str::<Vec<String>>(defns[idx])
+        .unwrap()
         .iter()
         .filter(|&s| !s.trim().is_empty())
         .for_each(|s| println!("{}\t{}", word, &s));
@@ -26,7 +25,7 @@ fn main() -> io::Result<()> {
     let hm = make_words();
     let words = hm.words;
     let ds = hm.defns;
-    let args = Cli::from_args();
+    let args = Cli::parse();
     if args.words.is_empty() {
         let filenames: Vec<&str> = vec![];
         let fileinput = FileInput::new(&filenames);
@@ -35,7 +34,7 @@ fn main() -> io::Result<()> {
             let w = line.unwrap();
             let idx = match words.binary_search(&w.as_str()) {
                 Ok(idx) => idx,
-                Err(_) => continue
+                Err(_) => continue,
             };
             print_defns(&ds, idx, &w);
             println!();
